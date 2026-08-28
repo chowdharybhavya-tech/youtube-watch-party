f5dea89623022028e5fff11c5bcbd0337164bc91
Bhavya Kumari <chowdharybhavya54@gmail.com>
2026-08-29 00:28:10 +0530

    Fix Socket.IO connection

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,8 +1,11 @@
 import { io } from "socket.io-client";
 
 const socket = io("https://youtube-watch-party-1ihc.onrender.com", {
-  transports: ["websocket", "polling"],
+  transports: ["polling"],
   withCredentials: true,
+  reconnection: true,
+  reconnectionAttempts: 10,
+  reconnectionDelay: 2000,
 });
 
 export default socket;
