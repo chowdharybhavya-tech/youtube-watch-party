9aa0ee2bbace679b76178f1022a8963d582eeed5
Bhavya Kumari <chowdharybhavya54@gmail.com>
2026-08-29 00:17:43 +0530

    Connect frontend to deployed backend

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,8 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
-  transports: ["websocket"],
+const socket = io("https://youtube-watch-party-1ihc.onrender.com", {
+  transports: ["websocket", "polling"],
+  withCredentials: true,
 });
 
 export default socket;
