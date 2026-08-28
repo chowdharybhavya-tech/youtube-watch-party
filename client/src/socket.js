c59e390a24741a2371c00f74813ac64747e055f3
Bhavya Kumari <chowdharybhavya54@gmail.com>
2026-08-29 00:39:47 +0530

    Fix frontend Socket.IO connection

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 const socket = io("https://youtube-watch-party-1ihc.onrender.com", {
-  transports: ["polling"],
+  transports: ["polling", "websocket"],
   withCredentials: true,
   reconnection: true,
   reconnectionAttempts: 10,
